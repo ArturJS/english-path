2339f673a0a9832819f5f80c70c5ee9e1a7828cf
ArturJS <nizamutdin.art@gmail.com>
2020-04-12 23:33:20 +0400

    backend: Added basic GraphQL authentication

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
-import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersResolver } from './users.resolver';
+import { UsersResolver } from './users/users.resolver';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -11,7 +12,16 @@ import { UsersResolver } from './users.resolver';
       installSubscriptionHandlers: true,
       autoSchemaFile: 'schema.gql',
       context: ({ req }) => ({ req }),
+      playground: {
+        settings: {
+          // wow it works!!!!
+          // https://github.com/prisma-labs/graphql-playground/issues/748#issuecomment-412524510
+          "request.credentials": "same-origin",
+        }
+      }
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService, UsersResolver],
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+    users: { id: number; email: string; password: string; }[];
+
+    constructor() {
+        this.users = [
+            {
+                id: 1,
+                email: 'user1@mail.com',
+                password: '123'
+            },
+            {
+                id: 2,
+                email: 'user2@mail.com',
+                password: '123'
+            }
+        ];
+    }
+
+    async findOne({email}) {
+        return this.users.find(u => u.email === email) || null;
+    }
+
+    async findById(userId) {
+        return this.users.find(u => u.id === userId) || null;
+    }
+}
